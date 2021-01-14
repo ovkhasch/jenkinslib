971aac5c6f029ef9775692197ab61cad8ab13251
ovkhasch <vkolegkv@gmail.com>
2021-01-14 06:27:48 +0300

    Update commitVersionGit.groovy

--- vars/commitVersionGit.groovy
+++ vars/commitVersionGit.groovy
@@ -2,6 +2,8 @@ def call(String version, String scope) {
   if (version != '') {      
     sh "git commit -a -m 'updating version to ${version}'"
     sh "git tag ${version}"
+    sh 'git config --local credential.helper "!f() { echo username=$GIT_AUTH_USR; echo password=$GIT_AUTH_PSW; }; f"'
     sh "git push"
+    sh "git config --local --unset credential.helper"
   }
 }
