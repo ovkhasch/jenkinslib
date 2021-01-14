b4f0e3df5a8ee6b4846deb4ac7e44715251b18b5
ovkhasch <vkolegkv@gmail.com>
2021-01-14 05:40:15 +0300

    Update setVersionMvn.groovy

--- vars/setVersionMvn.groovy
+++ vars/setVersionMvn.groovy
@@ -2,7 +2,7 @@ def call(String version, String scope) {
   if (version != '') {
     major = scope == 'major' ? 'true' : 'false'
     minor = scope == 'major' || scope == 'minor' ? 'true' : 'false'
-    sh "mvn versions:set -DnewVersion=${version} -Dincludes=com.kaiburr:*,org.cloudply:* -s settings.xml"
+    sh "mvn versions:set -DnewVersion=${version} -Dincludes=com.kaiburr:*,org.cloudply:* -DgenerateBackupPoms=false -s settings.xml"
     sh "mvn versions:use-latest-versions -DallowMajorUpdates=${major} -DallowMinorUpdates=${minor}  -Dincludes=org.cloudply:*,com.kaiburr:* -s settings.xml"
   }
 }
