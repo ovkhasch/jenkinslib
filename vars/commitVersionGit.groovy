f1ecbea611f064234c9b94d02cbb338e530f4cd6
ovkhasch <vkolegkv@gmail.com>
2021-01-14 05:50:35 +0300

    Update commitVersionGit.groovy

--- vars/commitVersionGit.groovy
+++ vars/commitVersionGit.groovy
@@ -1,6 +1,6 @@
 def call(String version, String scope) {
   if (version != '') {      
-    sh "git commit -m 'updating version to ${version}'"
+    sh "git commit -a -m 'updating version to ${version}'"
     sh "git tag ${version}"
     sh "git push"
   }
