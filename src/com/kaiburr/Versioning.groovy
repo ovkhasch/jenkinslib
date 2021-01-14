57b1dd99bd03b33c41a85ea12102f4ca0af7c277
ovkhasch <vkolegkv@gmail.com>
2021-01-14 04:39:24 +0300

    Update Versioning.groovy

--- src/com/kaiburr/Versioning.groovy
+++ src/com/kaiburr/Versioning.groovy
@@ -1,16 +1,16 @@
 void setVersionMvn(String version, String scope) {
   if (version != '') {
-      major = scope == 'major' ? 'true' : 'false'
-      minor = scope == 'major' || scope == 'minor' ? 'true' : 'false'
-      sh "mvn versions:set -DnewVersion=${version} -Dincludes=com.kaiburr:*;org.cloudply:* -s settings.xml"
-      sh "mvn versions:use-latest-versions -DallowMajorUpdates=${major} -DallowMinorUpdates=${minor}  -Dincludes=org.cloudply:*,com.kaiburr:* -s settings.xml"
+    major = scope == 'major' ? 'true' : 'false'
+    minor = scope == 'major' || scope == 'minor' ? 'true' : 'false'
+    sh "mvn versions:set -DnewVersion=${version} -Dincludes=com.kaiburr:*;org.cloudply:* -s settings.xml"
+    sh "mvn versions:use-latest-versions -DallowMajorUpdates=${major} -DallowMinorUpdates=${minor}  -Dincludes=org.cloudply:*,com.kaiburr:* -s settings.xml"
   }
 }
 
 void commitVersionGit(String version, String scope) {
-  if (version != '') {
-      
-      sh "git commit "
-      sh "mvn versions:use-latest-versions -DallowMajorUpdates=${major} -DallowMinorUpdates=${minor}  -Dincludes=org.cloudply:*,com.kaiburr:* -s settings.xml"
+  if (version != '') {      
+    sh "git commit -m 'updating version to ${version}'"
+    sh "git tag ${version}"
+    sh "git push"
   }
 }
