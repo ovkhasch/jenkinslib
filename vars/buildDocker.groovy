71f8432a7185ad74badbb215cf0ec8b924488efc
ovkhasch <vkolegkv@gmail.com>
2021-01-19 04:39:51 +0300

    Update buildDocker.groovy

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -1,6 +1,26 @@
   
-def call(String version, List<String> modules) {
+def call(String repo, String args, String version) {
   def image
+
+  if (version != '') {      
+    docker.withRegistry('', 'docker-hub') {
+      image = docker.build("""${repo}:${version}""", args)
+    }
+    docker.withRegistry('https://registry.kaiburr.com', 'chef01-registry') {
+      image.push()
+      if (BRANCH_NAME == 'master') {
+        image.push('latest')
+      }
+    }
+  } else {
+    docker.withRegistry('', 'docker-hub') {
+      image = docker.build(repo, args)
+    }
+    docker.withRegistry('https://registry.kaiburr.com', 'chef01-registry') {
+      image.push()
+    }      
+  }
+/*
   env.dockerfile = 'src/main/docker/Dockerfile.jvm'
 
   modules.each {
@@ -24,4 +44,5 @@ def call(String version, List<String> modules) {
         }      
       }
   }
+  */
 }
